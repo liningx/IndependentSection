ea2c069fca60bc56d3fc5e0e3a0673b9f79fa597
LEO <lining.x@gmail.com>
2016-12-14 17:51:38 +0800

    [leo]#N/A生成20以内的加减算式.

---
+++ section4/formula.ps1
@@ -0,0 +1,24 @@
+﻿$list = New-Object System.Collections.ArrayList
+
+function Get-formula($a, $b) {
+    $c = $a - $b
+    $d = $a + $b
+    
+    if ($c -gt -1){
+        $list.Add("$a - $b =")
+    }
+
+    if ($d -lt 21){
+        $list.Add("$a + $b =")
+    }
+}
+
+for ($i = 0; $i -le 20; $i ++) {
+    for ($j =0; $j -le 20; $j ++) {
+        Get-formula $i $j
+    }
+}
+
+$inputO = 0 .. ($list.Count - 1)
+
+Get-Random -InputObject $inputO -Count $list.Count | %{ $list[$_] }
